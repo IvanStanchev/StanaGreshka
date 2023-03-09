a2214a41e9d2e157d1d30b4ddd2792800d535fe1
Zlatina Lilova <zlati.lilova@gmail.com>
2023-03-09 20:47:50 +0200

    Update data_fetch.js

--- Server/data_fetch.js
+++ Server/data_fetch.js
@@ -1,72 +1,27 @@
 const { Data } = "dataclass";
 const { ReadlineParser } = require("serialport");
 
-<<<<<<< Updated upstream
-var dataClass = require("dataclass").Data;
-
-class Sensor extends dataClass {
-  temperature;
-  pressure;
-  humidity;
-  gas;
-}
-
-=======
->>>>>>> Stashed changes
 var SerialPort = require("serialport").SerialPort;
 var serialPort = new SerialPort({ path: "COM8", baudRate: 115200 });
 
 const parser = new ReadlineParser();
 
-<<<<<<< Updated upstream
-String.prototype.replaceAt = function (index, replacement) {
-  return (
-    this.substring(0, index) +
-    replacement +
-    this.substring(index + replacement.length)
-  );
-};
-
-serialPort.pipe(parser);
-// //parser.on("data", console.log);
-let json = "";
-let counter = 0;
-parser.on("data", function (data) {
-  json += data;
-  console.log("JSON \n" + json);
-  counter++;
-
-  console.log("If statement");
-  const jsonStr = json.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
-  console.log(jsonStr);
-  const jsonObj = JSON.parse(jsonStr);
-  console.log(jsonObj);
-  storeData(jsonObj);
-  json = "";
-});
-=======
 serialPort.pipe(parser);
 
-function ReadFromDevice() {
+function ReadData() {
   let json = "";
-  let counter = 0;
   parser.on("data", function (data) {
     json += data;
     console.log("JSON \n" + json);
-    counter++;
-
-    console.log("If statement");
     const jsonStr = json.replace(/(['"])?([a-zA-Z0-9_]+)(['"])?:/g, '"$2": ');
     console.log(jsonStr);
     const jsonObj = JSON.parse(jsonStr);
     console.log(jsonObj);
-
     storeData(jsonObj);
+    WriteData(jsonObj);
     json = "";
-    return jsonObj;
   });
 }
->>>>>>> Stashed changes
 
 let dataArray = [];
 function storeData(data) {
@@ -74,22 +29,26 @@ function storeData(data) {
   console.log("DATA");
   console.log(dataArray);
 }
-<<<<<<< Updated upstream
-=======
 
-function SignalDevice(json) {
-  let temperature = json.Data.temperature;
-  //analyse the data from the sensors
-  if (temperature > 20) {
+function WriteData(json) {
+  let CO2 = json.gas;
+  if (CO2 > 215) {
     serialPort.write("alarm", (err) => {
       if (err) {
-        console.error("Error writing to serial port:", err);
+        console.error("Error sending signal:", err);
       } else {
-        console.log("Data written to serial port");
+        console.log("Signal sent to start");
+      }
+    });
+  } else {
+    serialPort.write("extinguished", (err) => {
+      if (err) {
+        console.error("Error sending signal:", err);
+      } else {
+        console.log("Signal sent to end");
       }
     });
   }
 }
 
-ReadFromDevice();
->>>>>>> Stashed changes
+ReadData();
