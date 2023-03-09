335f979641fc0d1c03ce255aca48d666d9a3731e
Ivan Stanchev <ivan.stanchev04@gmail.com>
2023-03-09 16:54:34 +0200

    Add hedera client configuration

---
+++ server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+
+const inquirer = require("inquirer");
+
+const {
+    Client,
+} = require("@hashgraph/sdk");
+
+const questions = require("./utils.js").initQuestions;
+const log = require("./utils.js").handleLog;
+
+const hederaClient = Client.forTestnet();
+let logStatus = "Default";
+
+
+async function init() {
+    inquirer.prompt(questions).then(async function (answers) {
+        try {
+            logStatus = answers.status;
+            configureAccount(answers.account, answers.key);
+            console.log("Account configured successfully")
+        } catch (error) {
+            log("ERROR: init() failed", error, logStatus);
+            process.exit(1);
+        }
+    });
+}
+
+function configureAccount(account, key) {
+    try {
+      if (account === "" || key === "") {
+        log("init()", "using default .env config", logStatus);
+        operatorAccount = process.env.ACCOUNT_ID;
+        hederaClient.setOperator(process.env.ACCOUNT_ID, process.env.PRIVATE_KEY);
+      }
+      else {
+        operatorAccount = account;
+        hederaClient.setOperator(account, key);
+      }
+    } catch (error) {
+      log("ERROR: configureAccount()", error, logStatus);
+      process.exit(1);
+    }
+  }
+
+  init();
