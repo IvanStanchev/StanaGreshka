6ce1719e912bb1f4201a538bc5537cb93ccea139
Elena Varbanova <elena.varbanova04@gmail.com>
2023-03-10 10:50:51 +0200

    Fix authenticate error

--- Server/call_twilio.js
+++ Server/call_twilio.js
@@ -2,7 +2,7 @@
 const twilio = require('twilio');
 
 const accountSid = 'AC744144bb0ae77962619a975737dabafe';
-const authToken = '734aa293289fd99f87282131827a8014';
+const authToken = '10f2f44a392a87b6fa6c75655c07373f';
 
 const client = require('twilio')(accountSid, authToken);
 
@@ -11,7 +11,7 @@ function sendSMS() {
         .create({
             body: 'Alarm authorities',
             to: '+359885905045',
-            from: '+15674831739',
+            from: '+1 567 483 1739',
         })
         .then((message) => console.log(message.sid))
         .catch((error) => console.log(error));
